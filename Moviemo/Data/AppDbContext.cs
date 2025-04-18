910cea323ba8cd7a490221000cea6652ee7d5335
= <=>
2025-04-18 13:00:36 +0300

    AppDbContext oluşturuldu. Veritabanı bağlantısı yapıldı.

---
+++ Moviemo/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Moviemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.User> Users { get; set; }
+        public DbSet<Models.Movie> Movies { get; set; }
+        public DbSet<Models.Review> Reviews { get; set; }
+        public DbSet<Models.Comment> Comments { get; set; }
+        public DbSet<Models.Report> Reports { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Moviemo/Program.cs
+++ Moviemo/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Moviemo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
