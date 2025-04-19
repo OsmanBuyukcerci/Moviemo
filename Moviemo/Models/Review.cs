3393fece8430afb7b49bd0f96b4d8bc56d88ad60
= <=>
2025-04-19 13:11:57 +0300

    Eksik Review sınıfı satırları eklendi.

--- Moviemo/Models/Review.cs
+++ Moviemo/Models/Review.cs
@@ -4,6 +4,8 @@
     {
         public long reviewId { get; set; }
         public string body { get; set; }
+        public User author { get; set; }
+        public Movie reviewedMovie { get; set; }
         public double userScore { get; set; }
         public long likeCounter { get; set; }
         public long dislikeCounter { get; set; }
