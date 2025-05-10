b12692d98ca1ea4055703d31049a70454fc73708
= <=>
2025-05-10 19:58:47 +0300

    Cascade delete etkinleştirildi

--- Moviemo/Data/AppDbContext.cs
+++ Moviemo/Data/AppDbContext.cs
@@ -24,31 +24,31 @@ namespace Moviemo.Data
                 .HasOne(C => C.User)
                 .WithMany(U => U.Comments)
                 .HasForeignKey(U => U.UserId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Models.Comment>()
                 .HasOne(C => C.Movie)
                 .WithMany(M => M.Comments)
                 .HasForeignKey(C => C.MovieId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Models.Report>()
                 .HasOne(R => R.User)
                 .WithMany(U => U.Reports)
                 .HasForeignKey(R => R.UserId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Models.Review>()
                 .HasOne(R => R.User)
                 .WithMany(U => U.Reviews)
                 .HasForeignKey(R => R.UserId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Models.Review>()
                 .HasOne(R => R.Movie)
                 .WithMany(M => M.Reviews)
                 .HasForeignKey(R => R.MovieId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Models.Vote>()
                 .HasOne(V => V.User)
@@ -60,7 +60,7 @@ namespace Moviemo.Data
                 .HasOne(V => V.Comment)
                 .WithMany(C => C.Votes)
                 .HasForeignKey(V => V.CommentId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
