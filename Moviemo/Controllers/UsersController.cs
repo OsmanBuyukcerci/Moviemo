8781315a111376e08c4c6004ee1700c7dbc99abf
= <=>
2025-05-28 12:57:34 +0300

    Kullanıcının sadece kendi profilini güncellemesine izin verildi.

--- Moviemo/Controllers/UsersController.cs
+++ Moviemo/Controllers/UsersController.cs
@@ -54,7 +54,7 @@ namespace Moviemo.Controllers
                 return StatusCode(500, "Kullanıcı oluşturulurken bir sunucu hatası meydana geldi.");
 
             if (ResponseDto.IsCreated)
-                return Ok(Dto);
+                return Ok(ResponseDto);
 
             return ResponseDto.Issue switch
             {
